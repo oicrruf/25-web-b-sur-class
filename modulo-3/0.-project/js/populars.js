eeed07d272377c542126ccd535440b669bb8fd7c
Víctor M. Reyes <victor.reyes@focusservices.com>
2023-06-19 20:18:17 -0600

    feat: creación de buscador

--- modulo-3/0.-project/js/populars.js
+++ modulo-3/0.-project/js/populars.js
@@ -1,25 +1,9 @@
 const popularBox = document.querySelector("#popular");
 let allMovies = [];
 
-const getPopular = async () => {
-  const response = await fetch(
-    "https://api.themoviedb.org/3/movie/popular?language=es&page=1",
-    {
-      // method: 'get',
-      headers: new Headers({
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTNmMzMyMzkxMmEzN2QwYmY4MDBmYjhmODYyYWM2MiIsInN1YiI6IjYyMTVjMzc0YmQxMDFlMDAxYmM4NTc0MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KGWT_v3k4KetupTNHFXYXZjbSMn89goD0u8cdG0hrSw",
-      }),
-    }
-  );
-  // console.log(1)
-
-  return await response.json();
-};
-
-getPopular().then((response) => {
-  allMovies = response.results;
-  response.results.forEach((movie) => {
+// Renderizado de películas 
+const renderizarPeliculas = (listado) => {
+  listado.forEach((movie) => {
     const myDiv = `<div class="col-lg-3 mb-4">
         <a href="#" class="">
           <img class="img-fluid rounded-2" src="https://image.tmdb.org/t/p/w440_and_h660_face/${movie.poster_path}"/>
@@ -31,17 +15,62 @@ getPopular().then((response) => {
       </div>`;
     popularBox.innerHTML += myDiv;
   });
+}
+
+// Obtener películas populares
+const getPopular = async () => {
+  const response = await fetch(
+    "https://api.themoviedb.org/3/movie/popular?language=es&page=1",
+    {
+      // method: 'get',
+      headers: new Headers({
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTNmMzMyMzkxMmEzN2QwYmY4MDBmYjhmODYyYWM2MiIsInN1YiI6IjYyMTVjMzc0YmQxMDFlMDAxYmM4NTc0MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KGWT_v3k4KetupTNHFXYXZjbSMn89goD0u8cdG0hrSw",
+      }),
+    }
+  );
+
+  return response.json();
+};
+
+// console.log(getPopular())
+
+
+getPopular().then((response) => {
+  allMovies = response.results;
+  renderizarPeliculas(allMovies)
 });
 
-const movies = [1, 2, 3, 4, 5];
+// Inicia código del buscador
 
-const mayoresDeTres = movies.filter((movie) => movie > 3);
+const formulario = document.querySelector('#search')
 
-console.log(mayoresDeTres);
+const buscador = (search) => {
+  popularBox.innerHTML = ''
+  let a_minusculas = search.toLowerCase();
+   let resultado = allMovies.filter((movie_name) => movie_name.title.toLowerCase().includes(a_minusculas));
+   renderizarPeliculas(resultado)
+};
 
-const names = ["Victor", "Jesus", "Ana", "Maria", "Elias", "Ramon", "Mario"];
+formulario.addEventListener("submit", (event)=> {
+  event.preventDefault()
+  let valor_buscado = event.target["movie"].value
+  buscador(valor_buscado)
+})
 
-const buscador = (search) =>
-  names.filter((name) => name.toLowerCase().includes(search.toLowerCase()));
+
+
+// const movies = [1, 2, 3, 4, 5];
+
+// const mayoresDeTres = movies.filter((movie) => movie > 3);
+
+// console.log(mayoresDeTres);
+
+// const names = ["Victor", "Jesus", "Ana", "Maria", "Elias", "Ramon", "Mario"];
+
+// const buscador = (search) => {
+//   let a_minusculas = search.toLowerCase();
+//   return names.filter((name) => name.toLowerCase().includes(a_minusculas));
+// };
 
 // console.log(mayoresDeTres)
