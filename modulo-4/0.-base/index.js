f78a209e58a03cd4478743baf4e7ca9bf138e2c3
Víctor M. Reyes <victor.reyes@focusservices.com>
2023-07-12 20:47:23 -0600

    chore: delete node_modules

--- modulo-4/0.-base/index.js
+++ modulo-4/0.-base/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.status(401).json({"message": "Server running"})
+  res.status(200).json({"message": "Server running"})
 })
 
 app.listen(8000)
