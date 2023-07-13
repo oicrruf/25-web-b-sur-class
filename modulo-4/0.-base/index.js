0fb8bf64379ca4a7a4aa8aef4ab846810fbf9ad9
Víctor M. Reyes <victor.reyes@focusservices.com>
2023-07-12 21:22:47 -0600

    chore: params and query params

--- modulo-4/0.-base/index.js
+++ modulo-4/0.-base/index.js
@@ -1,8 +1,43 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const port = 8000;
 
-app.get('/', function (req, res) {
-  res.status(200).json({"message": "Server running"})
-})
+const alumnos = [
+  { id: 1, name: "Anayeli Soto", score: 40 },
+  { id: 2, name: "Efrén Jiménez", scrore: 50 },
+  { id: 3, name: "Erika Hernández", score: 60 },
+  { id: 4, name: "Elias Monrroy", score: 70 },
+];
 
-app.listen(8000)
+app.get("/", function (req, res) {
+  res.status(200).json({ message: "Server running" });
+});
+
+app.get("/alumnos", function (req, res) {
+  res.status(200).json(alumnos);
+});
+
+// http://localhost:8000/alumnos/notas?min=50&max=60
+app.get("/alumnos/notas/", function (req, res) {
+  console.log(req.query)
+  res.status(200).json(alumnos);
+});
+
+app.get("/alumnos/:id", function (req, res) {
+  let alumno = alumnos.filter(
+    (alumno) => alumno.id === parseInt(req.params.id)
+  );
+  if (alumno.length !== 0) {
+    res.status(200).json(alumno[0]);
+  } else {
+    res.status(404).json({ message: "Alumno no encontrado" });
+  }
+});
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server running in port ${port}`);
+});
